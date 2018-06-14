3a8adaa56fd843bf30ffb8f359cd0de416d6477b
Scott Skow <sskow@layer3tv.com>
2018-06-14 12:58:09 -0600

    fixing install scripts

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PATH=$PATH:./node_modules/.bin
-npm install -g @vue/cli-service-global
-cd bitbooks-client && npm install --verbose && npm run build
+#npm install -g @vue/cli-service-global
+cd bitbooks-client && npm install && npm run build
