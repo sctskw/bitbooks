0af9fc0bc21a9ba4adc614b32d75f991a6d648d4
Scott Skow <sskow@layer3tv.com>
2018-06-14 15:06:39 -0600

    socket server up and running

--- bitbooks-api/server.js
+++ bitbooks-api/server.js
@@ -1,14 +1,25 @@
 const path = require('path');
 const restify = require('restify');
 const API = require('./main.js');
+const SOCKET = require('./socket.js');
 
+//TOOD: move to CONFIG
 const APP = 'bitbooks';
+const APP_BASE = process.env.APP_BASE || __dirname;
+const API_SERVER = '0.0.0.0';
+const API_PORT = process.env.PORT || 11001;
+const SOCKET_SERVER = API_SERVER;
+const SOCKET_PORT = process.env.PORT || 11002;
 
 //initialize the server
 const server = restify.createServer({
   name: APP,
   strictRouting: false
 });
+
+//configure it
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
 
 //mount the API
 //NOTE: must do this first
@@ -17,11 +28,27 @@ API.mount(server, '/api');
 //mount the web app base directory
 server.get('*', restify.plugins.serveStatic({
   //TODO: how to deal with relative pathing? aliases? globals?
-  directory: path.resolve(process.env.APP_BASE, 'bitbooks-client/dist'),
+  directory: path.resolve(APP_BASE, 'bitbooks-client/dist'),
   default: 'index.html'
 }));
 
 //start the server
-server.listen(process.env.PORT || 11001, '0.0.0.0', function() {
+server.listen(API_PORT, API_SERVER, function() {
   console.log(`${server.name} listening at ${server.url}`);
+});
+
+//start the socket server
+SOCKET.serve({
+  server: SOCKET_SERVER,
+  port: SOCKET_PORT,
+
+  //TODO: connect these
+  handlers: {
+    connection: function(socket, req) {
+
+    },
+    message: function(message) {
+
+    }
+  }
 });
---
+++ bitbooks-api/socket.js
@@ -0,0 +1,34 @@
+const WebSocket = require('ws');
+
+const DEFAULT_SERVER = '0.0.0.0';
+const DEFAULT_PORT = 11002;
+
+module.exports.serve = function initSocketServer(config) {
+
+  //configs aren't required
+  if(!config) config = {};
+
+  //setup defaults
+  if(!config.server) config.server = process.env.SOCKET_SERVER || DEFAULT_SERVER;
+  if(!config.port) config.port = process.env.SOCKET_PORT || DEFAULT_PORT;
+
+  //create the server
+  const server = new WebSocket.Server({
+    server: config.server,
+    port: config.port
+  }, function() {
+    console.log(`bitbooks socket listening on ws://${config.server}:${config.port}`);
+  });
+
+  //connection established event
+  server.on('connection', function(socket, req) {
+    console.log(`incoming connection established: ${req.connection.remoteAddress}`);
+    socket.send('connection established. welcome!');
+  });
+
+  //incoming message event
+  server.on('message', function(message) {
+    console.log(`incoming message: ${message}`);
+  });
+
+}
