e6423422d52ae11c9a52a0d18435fee7a50314c9
Scott Skow <sskow@layer3tv.com>
2018-06-19 12:33:03 -0600

    fixing error in feed script

--- bitbooks-api/bin/start_feeds.sh
+++ bitbooks-api/bin/start_feeds.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 export APP_BASE=$PWD
-export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 (while true; do node $APP_BASE/bitbooks-api/lib/feeder --exchange=poloniex --market=BTC_ETH </dev/null; done) >> /dev/null &
 (while true; do node $APP_BASE/bitbooks-api/lib/feeder --exchange=bittrex --market=BTC-ETH </dev/null; done) >> /dev/null &
