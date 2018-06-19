ee5ee314f786f0c22cd2e0485a3101cbef4dcd71
Scott Skow <sskow@layer3tv.com>
2018-06-19 11:32:08 -0600

    fixing post install

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,6 +2,6 @@
 export PATH=$PATH:./node_modules/.bin
 export APP_BASE=$PWD
 
-cd $APP_BASE/bitbooks-api && npm run feeders
 cd $APP_BASE/bitbooks-client && npm run build
+cd $APP_BASE/bitbooks-api && npm run feeders
 
