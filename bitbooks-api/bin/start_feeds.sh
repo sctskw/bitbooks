520747928a0b5255f218125ef799b5cd363f2849
Scott Skow <sskow@layer3tv.com>
2018-06-19 11:51:41 -0600

    fixing feeder daemon

--- bitbooks-api/bin/start_feeds.sh
+++ bitbooks-api/bin/start_feeds.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+export APP_BASE=$PWD
 
-# (while true; do node ./feeder --exchange=poloniex --market=BTC_ETH </dev/null; done) >> /dev/null &
-# (while true; do node ./feeder --exchange=bittrex --market=BTC-ETH </dev/null; done) >> /dev/null &
+(while true; do node ./lib/feeder --exchange=poloniex --market=BTC_ETH </dev/null; done) >> /dev/null &
+(while true; do node ./lib/feeder --exchange=bittrex --market=BTC-ETH </dev/null; done) >> /dev/null &
 
-node ./lib/feeder --exchange=poloniex --market=BTC_ETH &
-node ./lib/feeder --exchange=bittrex --market=BTC-ETH &
+#node ./lib/feeder --exchange=poloniex --market=BTC_ETH &
+#node ./lib/feeder --exchange=bittrex --market=BTC-ETH &
