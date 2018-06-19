5fc7c08edebb3c9c05b3375d59bdb4cffbc6e4cb
Scott Skow <sskow@layer3tv.com>
2018-06-19 12:26:40 -0600

    fixing feeder pwd

--- bitbooks-api/bin/start_feeds.sh
+++ bitbooks-api/bin/start_feeds.sh
@@ -2,7 +2,7 @@
 export APP_BASE=$PWD
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-EXEC=$SCRIPT_DIR/../lib/feeder
+EXEC=$APP_BASE/bitbooks-api/lib/feeder.js
 
 (while true; do node $EXEC --exchange=poloniex --market=BTC_ETH </dev/null; done) >> /dev/null &
 (while true; do node $EXEC --exchange=bittrex --market=BTC-ETH </dev/null; done) >> /dev/null &
