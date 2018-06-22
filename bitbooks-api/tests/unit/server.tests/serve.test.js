7e517e3de875134028d28c4bdf6c9f11e8aaca86
Scott Skow <sskow@layer3tv.com>
2018-06-22 16:20:29 -0400

    adding server serve tests

---
+++ bitbooks-api/tests/unit/server.tests/serve.test.js
@@ -0,0 +1,97 @@
+/** __mocks__ **/
+
+// const MOCK_CONFIG = {
+//   APP: 'test',
+//   APP_BASE: __dirname,
+//   API_SERVER: '1.1.1.1',
+//   API_PORT: 2
+// }
+
+function doMocks () {
+  jest.resetModules()
+  jest.mock('config.js')
+  jest.mock('server/main.js')
+  jest.mock('server/socket.js')
+  jest.mock('lib/cache')
+  jest.mock('bin/feeds.js')
+
+  jest.genMockFromModule('config.js')
+  jest.genMockFromModule('server/main.js')
+  jest.genMockFromModule('server/socket.js')
+  jest.genMockFromModule('lib/cache')
+  jest.genMockFromModule('bin/feeds.js')
+}
+
+describe('non-production tests', () => {
+  beforeEach(() => {
+    doMocks()
+    process.env.NODE_ENV = 'something'
+  })
+
+  afterEach(() => {
+    delete process.env.NODE_ENV
+  })
+
+  test('server/serve starts the server', (done) => {
+    expect.hasAssertions()
+
+    const SERVER = require('server/main.js')
+    const SOCKET = require('server/socket.js')
+    const STORAGE = require('lib/cache')
+    const FEEDS = require('bin/feeds.js')
+
+    SERVER.listen.mockImplementation(jest.fn())
+    SOCKET.serve.mockImplementation(jest.fn().mockResolvedValue({}))
+    STORAGE.subscribe.mockImplementation(jest.fn())
+    FEEDS.start.mockImplementation(jest.fn())
+
+    // init the server
+    require('server')
+
+    // TODO: wait for actual state versus abitrary time
+    // NOTE: this could cause intermittent failures during test
+    // suite runs
+    setTimeout(() => {
+      expect(SERVER.listen).toBeCalled()
+      expect(STORAGE.subscribe).toBeCalled()
+      expect(FEEDS.start).not.toBeCalled()
+      done()
+    }, 50)
+  })
+})
+
+describe('production tests', () => {
+  beforeEach(() => {
+    doMocks()
+    process.env.NODE_ENV = 'production'
+  })
+
+  afterEach(() => {
+    delete process.env.NODE_ENV
+  })
+
+  test('server/serve starts the server and feeds', (done) => {
+    const SERVER = require('server/main.js')
+    const SOCKET = require('server/socket.js')
+    const STORAGE = require('lib/cache')
+    const FEEDS = require('bin/feeds.js')
+
+    SERVER.listen.mockImplementation(jest.fn())
+    SOCKET.serve.mockImplementation(jest.fn().mockResolvedValue({}))
+    STORAGE.subscribe.mockImplementation(jest.fn())
+    FEEDS.start.mockImplementation(jest.fn())
+
+    // init the server
+    require('server')
+
+    // TODO: wait for actual state versus abitrary time
+    // NOTE: this could cause intermittent failures during test
+    // suite runs
+    setTimeout(() => {
+      expect(SERVER.listen).toBeCalled()
+      expect(STORAGE.subscribe).toBeCalled()
+      expect(FEEDS.start).toBeCalled()
+      done()
+    }, 50)
+  })
+})
