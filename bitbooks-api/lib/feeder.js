6fe3b551775ff79b540a1ff51f0429a7208ac64a
Scott Skow <sskow@layer3tv.com>
2018-06-24 22:53:27 -0400

    updated feeder docs

--- bitbooks-api/lib/feeder.js
+++ bitbooks-api/lib/feeder.js
@@ -1,14 +1,26 @@
 #!/usr/bin/env node
+
+/**
+ * Wrapper module to start a feed for an available Exchange Client
+ *
+ * Usage:
+ *
+ * ./feeder --exchange=bittrex --market=BTC_ETH
+ *
+ */
 const path = require('path')
 
+// useful to find the base level directory in child processes
 global.__appbase = path.resolve(__dirname, '..')
 
 const Parser = require('minimist')
 const Cache = require('./cache/storage')
 const Clients = require('./clients')
 
+// parse the command line arguments
 const ARGS = Parser(process.argv, {'--': true})
 
+// create the storage cache
 const Storage = new Cache()
 
 Clients.subscribe({
@@ -16,6 +28,7 @@ Clients.subscribe({
   market: ARGS.market
 }, function (message) {
   try {
+    // execute the storage method provided by the message
     Storage[message.type](message)
   } catch (err) {
     console.error(`fail: ${err}`)
