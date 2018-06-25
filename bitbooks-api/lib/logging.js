302d63444b6437f03778dcabcfb26ff528ca469a
Scott Skow <sskow@layer3tv.com>
2018-06-24 22:49:27 -0400

    added logger usage

--- bitbooks-api/lib/logging.js
+++ bitbooks-api/lib/logging.js
@@ -1,9 +1,20 @@
+/**
+ * Simple console.log wrapper that gives each log a readable namespace
+ *
+ *  Usage:
+ *
+ *  let log = require('logging')('mymodule')
+ *
+ *  log('something')
+ *
+ *  > '[mymodule] something'
+ *
+ */
 module.exports = function getLog (namespace) {
   return function logger (msg, data) {
-
     let text = `[${namespace}] ${msg}`
 
-    if(!data) return console.log(text)
+    if (!data) return console.log(text)
 
     console.log(text, JSON.stringify(data))
   }
