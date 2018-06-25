202314f6f585b6508c2a99155ba361d9fca5e72b
Scott Skow <sskow@layer3tv.com>
2018-06-24 23:42:51 -0400

    updating external css requires

--- bitbooks-client/src/views/App/index.vue
+++ bitbooks-client/src/views/App/index.vue
@@ -1,4 +1,5 @@
 <style src="../../assets/roboto.icons.css"></style>
+<style src="../../../node_modules/vuetify/dist/vuetify.min.css"></style>
 <template src="./template.html"></template>
 <style lang="scss" src="./style.scss"></style>
 <script src="./script.js"></script>
--- bitbooks-client/src/views/App/script.js
+++ bitbooks-client/src/views/App/script.js
@@ -1,6 +1,3 @@
-// require('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons')
-require('../../../node_modules/vuetify/dist/vuetify.min.css')
-
 import Status from '@/components/Status/index.vue'
 
 export default {
